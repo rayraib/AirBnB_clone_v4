a9b521c7d025f7ef5d9ec26ffdd57bbc30cd4aad
rayraib <binitarai11@gmail.com>
2018-05-13 04:38:09 +0000

    dynamically display amenities when checked

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,22 @@
-$( document ).ready(function () {
-    let amenities = []
-    $('input:checkbox').change(
-        function(){
-            if ($(this).is(':checked')){
-                amenities.push(this)
-            } else {
-                function remove(amenities, this) {
-                    return amenities.filter(e => e !== this);
-            }
-        });
-    let result = []
-    for (let i = 0; i < amenities.length; i++) {
-        let obj = JSON.stringify(amenities[i]);
-        result.push(obj)
-    }
-    $('.amenties h4').replaceWith(result)
+$(document).ready(function () {
+  let amenities = [];
+  $('input:checkbox').change(
+    function () {
+      console.log(this);
+      if ($(this).is(':checked')) {
+        amenities.push(this);
+      } else {
+        amenities = amenities.filter(e => e !== this);
+      }
+      let result = [];
+      for (let i = 0; i < amenities.length; i++) {
+        let name = $(amenities[i]).attr('data-name');
+        result.push(name);
+      }
+      if (result.length === 0) {
+        $('.amenities h4').html('&nbsp;');
+      } else {
+        $('.amenities h4').text(result.join(', '));
+      }
+    });
 });
