60868e8ea7383fdac2c8af3a6616b7dc4054f798
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-05-14 12:08:02 -0700

    Added in functionality to reload search results from database

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+  // checks to make sure api returns a 200 status code
+  let url = "http://0.0.0.0:5001/api/v1/status/";
+  $.get(url, function(data, textStatus) {
+    if (textStatus == 'success') {
+      $('#api_status').addClass("available");
+    }
+    else {
+      $('#api_status').removeClass("available");
+    }
+  });
+
+  // handles checked boxes
+  let amenities = [];
+  let myList = [];
+  $('input:checkbox').change(
+    function () {
+      if ($(this).is(':checked')) {
+        amenities.push(this);
+      } else {
+        amenities = amenities.filter(e => e !== this);
+      }
+      let result = [];
+      for (let i = 0; i < amenities.length; i++) {
+        let name = $(amenities[i]).attr('data-name');
+        //let myAttr = $(amenities[i]).attr('data-id');
+        result.push(name);
+        myList.push($(amenities[i]).attr('data-id'));
+        //console.log(name)
+        //console.log(myAttr)
+        //myList.push($(amenities[i]));
+      }
+      if (result.length === 0) {
+        $('.amenities h4').html('&nbsp;');
+      } else {
+        $('.amenities h4').text(result.join(', '));
+      }
+    });
+    // POST request for all places
+    
+  let amenitiesDict = {};        
+  let postUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+
+  // check if button has been pushed
+  function myRequest() {
+  $.ajax({
+    type: 'POST',
+    url: postUrl,
+    data: JSON.stringify(amenitiesDict),
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++){
+        let place = data[i];
+        $('.places').append('<article>' + 
+        '<div class="title">' +
+	      '<h2>' + (place['name']) + '</h2>' +
+	      '<div class="price_by_night">' +
+		    (place['price_by_night']) +
+
+	     '</div>' + 
+	    '</div>' +
+	    '<div class="information">' + 
+	      '<div class="max_guest">' +
+		'<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+		'<br />' + 
+		 (place['max_guest']) + 'Guests' +
+	      '</div>' +
+	      '<div class="number_rooms">' +
+		'<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+		'<br />' +
+		 (place['number_rooms']) + 'Bedrooms' +
+	      '</div>' + 
+	      '<div class="number_bathrooms">' +
+		'<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+		'<br />' +
+		(place['number_bathrooms']) + 'Bathroom' +
+	      '</div>' + 
+	    '</div>' + 
+	    '<div class="description">' +
+	      (place['description']) +
+        "</article>")
+    }
+  }
+  });
+};
+$(':button').click(function () {
+  amenitiesDict['amenities'] = myList;
+  myRequest();
+});
+
+  // POST request for clicked places
+//  $(':button').click(function () {
+//    let amenitiesDict = {}
+//    amenitiesDict['amenities'] = myList;
+/*    let postUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+    $.ajax({
+      type: 'POST',
+      url: postUrl,
+      data: JSON.stringify(amenitiesDict),
+      contentType: 'application/json',
+      success: function (data) {
+        window.location.reload(true);
+      //console.log(data);
+        for (let i = 0; i < data.length; i++){
+          let place = data[i];
+        //console.log(place['name']);
+          $('.places').append('<article>' + 
+          '<div class="title">' +
+	        '<h2>' + (place['name']) + '</h2>' +
+	        '<div class="price_by_night">' +
+		      (place['price_by_night']) +
+
+	        '</div>' + 
+	        '</div>' +
+	        '<div class="information">' + 
+	        '<div class="max_guest">' +
+		      '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+		      '<br />' + 
+		      (place['max_guest']) + 'Guests' +
+	        '</div>' +
+	        '<div class="number_rooms">' +
+		      '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+		      '<br />' +
+		      (place['number_rooms']) + 'Bedrooms' +
+	        '</div>' + 
+	        '<div class="number_bathrooms">' +
+	      	'<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+		      '<br />' +
+		      (place['number_bathrooms']) + 'Bathroom' +
+	        '</div>' + 
+	        '</div>' + 
+	        '<div class="description">' +
+	        (place['description']) +
+          "</article>")
+        }
+      } 
+    }); */
+  }); 
